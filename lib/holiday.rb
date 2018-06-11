f14f4d5abb77fa6a41f35ee52a07e5846fbc8e45
Jason Pitts <juniorjr083@gmail.com>
2018-06-11 01:39:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,24 +65,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
- holiday_supplies.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
-    data.each do |holiday, supply|
-      # holiday == :fourth_of_july
-      # we took the holiday key and converted a string
-      # str_holiday == fourth_of_july
-      # we took that converted string, and split it to an array with the elements divided by the _ (which removes the _)
-      # str_holiday == ["fourth", "of", "july"]
-      # we collected over that array in order to capitalize each word
-      # str_holiday == ["Fourth", "Of", "July"]
-      # we joined the elements together to form a string where each element is divded by an empty space
-      # str_holiday == "Fourth Of July"
-      str_holiday = holiday.to_s.split("_").collect {|word| word.capitalize!}.join(" ")
-      # supplies == ["BBQ", "Fireworks"]
-      # we joined it together converting the array to a string dividing each element by a , and a space
-      # supplies == "BBQ, Fireworks"
-      # "  Fourth Of July: BBQ, Fireworks"
-      puts "  #{str_holiday}: #{supply.join(", ")}"
+ holiday_supplies.each do |k, v|
+    puts "#{k.to_s.capitalize!}:"
+    v.each do |day, stuff|
+     
+      str_holiday = day.to_s.split("_").collect {|w| w.capitalize!}.join(" ")
+      
+      puts "  #{str_holiday}: #{stuff.join(", ")}"
     end
   end 
 end
