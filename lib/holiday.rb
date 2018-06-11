2bb171f06fa8b27d90c634ac69a91b6e84eed9d3
Jason Pitts <juniorjr083@gmail.com>
2018-06-11 01:30:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,6 +65,13 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.each do |k,v| 
+    puts k 
+    puts k v
+    
+    
+  end 
 
 end
 
